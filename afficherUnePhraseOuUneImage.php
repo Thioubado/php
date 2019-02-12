dc37f5e33d811820b4494754d9eba78a72931179
Unknown <seckndanane16@gmail.com>
2019-02-12 03:12:21 +0100

    Afficher une phrase ou une image en php

---
+++ afficherUnePhraseOuUneImage.php
@@ -0,0 +1,12 @@
+<?php
+    echo 'Bonjour le monde !';
+
+    echo '<br>';
+
+    echo "<font face = 'arial' size = '2' color = 'red' >Bonjour le monde </font>";
+
+    echo "<div align = 'center'><font face = 'arial' size='2' color='blue'>Bonjour le monde !</font><br>";
+    
+    echo "<img src='images/globe.gif' border='0' /></div>";
+
+?>
