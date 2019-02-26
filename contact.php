e978ea18ea6bdea6d346824a4826193b9e158e96
Unknown <seckndanane16@gmail.com>
2019-02-26 13:21:53 +0100

    php et html

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php 
+$nav = 'contact';
+$title = 'Nous contacter';
+require 'header.php';
+?>
+
+<h1>Nous contacter</h1>
+<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nulla unde eos non laboriosam voluptatem impedit iusto quaerat quod facere architecto, placeat voluptate voluptatibus debitis eum voluptatum ea maxime doloribus?</p>
+
+<?php require 'footer.php';?>
