ca3ab29777d45d67d9bc33100cfa8cb4a4cbf1dd
COTE <GrCOTE7@GMail.com>
2019-03-03 06:12:52 +0100

    Up $année

--- dashboard.php
+++ dashboard.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'header.php';
 $annees           = (int) date('Y');
 $annees_selection = empty($_GET['annee']) ? null : (int) $_GET['annee'];
@@ -7,15 +8,34 @@ $mois_selection   = empty($_GET['mois']) ? null : (int) $_GET['mois'];
 echo '<hr>
 <h1>
 </h1>Salut Momo: Ecris ICI qd tu es là ! :-)<br>
-- 
-
-
-
-
-
+- ok
+- regarde, g mis le cs-fixer... Va au début de la ligne 33 et fais 4 fois tabulation et on attend, tu va voir.... Tu as vu, le VSC a re-ranger bien l\'alignement des variables
+- oui j\'ai vu
+- Autre chose: Regarde je fais plein d\'espace et on attends 2-3 secondes...
+Pareil ! Le script a tout re ranger bien :-) !
+- oui c\'est cs-fixer !
+-ce truc est puissant c\'est une affaire de fou
+- Oui, bon regardons tes variables (juste toi, faut faire F5 qd tu veux voir sur la fenetre ngrok)....
+- Ok mais une petite question comment tu as fait pour trouver cs-fixer? 
+- Lors d\'un tuto, g vu un mec l\'utiliser... Ensuite: Github cs-fixer dans google
+- Ok je vois
+- Bien, donc, observons les vaiables...:!d'
 </h1>
 <hr>';
-var_dump($annees_selection, $mois_selection);
+
+var_dump($annees_selection, $mois_selection); // Cette ligne noous montre ce qu'elles contiennent
+// on voit que la permière est à null et la seconde contient 3
+// Donc, null rend false la condition dans tous les k avec un &&
+// S'il fait super bon && Quoi qu'il arrive, je n'irai pas à la place
+//  Alors, je vais à la plage
+// Bin le gars, il ira pas se baigner !!! Jamais
+// Là, c pareil
+// Pour que cette condition soit vrai il faut que les 2 parties retournent vrai (ou quelque chose)
+// Donc, faut surveiller $annees_selection, elle est à NULL et ne devrait certainement pas y être, en tous k, pas tout le temps...
+// C'est bon, je te laisse 'creuser' :) ?
+// D'aacord je comprends mieux 
+// Parfait, alors, je te laisse creuser la question pour que ça marche comme tu veux, moi, je push ce fichier tel qu'il est là, et juste un dble-clic dans GK te le donne instantannément dans ton VSC...
+
 if ($annees_selection && $mois_selection) {
     echo '<h1>Cette condition semble être jamais vraie</h1>';
     $total = nombre_vue_mois($annees_selection, $mois_selection);
