dff64154975d9443fa031055916964ffe1dd1866
Unknown <seckndanane16@gmail.com>
2019-03-25 01:24:32 +0100

    L'objet DateTime

--- cli/test.php
+++ cli/test.php
@@ -1,4 +1,17 @@
 <?php 
-$fichier = __DIR__ . DIRECTORY_SEPARATOR . 'text.odt';
+/*$fichier = __DIR__ . DIRECTORY_SEPARATOR . 'text.odt';
 //file_put_contents($fichier, ' Marc comment ça va ?', FILE_APPEND);
-echo $ressource = fopen($fichier, 'r+');
+echo $ressource = fopen($fichier, 'r+');*/
+
+$date = new DateTime();
+$date -> modify('+1 month');
+echo $date -> format('d-m-Y');
+//var_dump($date);
+
+$date1 = '2019-01-01';
+$date2 = '2019-04-01';
+
+$d = new DateTime($date1);
+$d2 = new DateTime($date2);
+$diff = $d->diff($d2, true);
+echo "il y a {$diff -> days} jours de différence";
